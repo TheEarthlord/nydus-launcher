96445409f272d2bced6653c2cfdc9f9155aef55e
Christopher Irving <the.earthlord3@gmail.com>
2025-07-27 18:00:04 +1000

    Split variable strings functions into their own file

---
+++ nydus-launcher/usr/lib/python3/dist-packages/nydus/client/varstrings.py
@@ -0,0 +1,103 @@
+
+# Functions for dealing with strings that contain
+# variables; e.g. ${varname}
+
+VARNAME_START = "${"
+VARNAME_END = "}"
+
+"""
+Returns True if name is a string and a variable for the JSON version
+purposes (i.e. starts with '${', ends with '}', and has something in
+between)
+"""
+def is_variable(name):
+    if not isinstance(name, str):
+        return False
+    if name.startswith(VARNAME_START):
+        if name.endswith(VARNAME_END):
+            nstart = len(VARNAME_START)
+            nend = len(name) - len(VARNAME_END)
+            middle = name[nstart:nend]
+            middle = middle.strip()
+            if middle:
+                return True
+    return False
+
+"""
+Returns True if arg is a string of the form
+"name=variable"
+where variable begin with VARNAME_START and end with VARNAME_END
+as in is_variable.
+Note that strings which make is_variable True will make this function False;
+this function requires something in front of the variable.
+"""
+def contains_variable(arg):
+    if not isinstance(arg, str):
+        return False
+    nstart = arg.find(VARNAME_START)
+    if nstart < 0:
+        return False
+    nend = arg.find(VARNAME_END)
+    if nend < 0:
+        return False
+
+    # Start must be before end
+    if nend <= nstart:
+        return False
+
+    if nstart > 0 and nend > nstart:
+        if arg[nstart - 1] == "=":
+
+            # Everything before the equals
+            prepart = arg[:nstart - 1]
+
+            # Changing nstart to the index after the end
+            # the VARSTART, rather than the index of the
+            # beginning
+            nstart += len(VARSTART)
+            middle = arg[nstart:nend]
+            
+            # We want the variable name and part before
+            # the variable to be nonempty
+            if prepart.strip() and middle.strip():
+                return True
+    return False
+    
+"""
+If arg is a string which is a variable or contains a variable
+(according to is_variable and contains_variable), then get
+the name of that variable (the part between VARNAME_START and
+VARNAME_END) and return it. Else, return empty string.
+"""
+def get_varname(arg):
+
+    if is_variable(arg) or contains_variable(arg):
+        nstart = arg.find(VARNAME_START) + len(VARNAME_START)
+        nend = arg.find(VARNAME_END)
+        middle = arg[nstart:nend]
+        return middle
+        
+    return ""
+
+"""
+arg: string, an argument containing a variable or which is a variable
+    according to is_variable or contains_variable
+newval: string, a value which will be placed where the variable was in
+    the argument, including removal of VARSTART and VAREND
+Returns the arg with newval placed where the variable was.
+If no variable can be found or newval is not a string, return empty string.
+"""
+def replace_varname(arg, newval):
+    if not isinstance(newval, str):
+        return ""
+
+    if is_variable(arg) or contains_variable(arg):
+        nstart = arg.find(VARNAME_START)
+        nend = arg.find(VARNAME_END) + len(VARNAME_END)
+        first_part = arg[:nstart]
+        last_part = arg[nend:]
+        result = first_part + newval + last_part
+        return result
+
+    return ""
+
