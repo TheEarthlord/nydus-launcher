1f9f1bf5a056f984bec127ade00c3366a3197cef
Christopher Irving <the.earthlord3@gmail.com>
2025-07-29 17:41:21 +1000

    Adding version output to all executables

--- nydus-launcher/usr/lib/python3/dist-packages/nydus/common/nydus_info.py
+++ nydus-launcher/usr/lib/python3/dist-packages/nydus/common/nydus_info.py
@@ -1,6 +1,29 @@
 
+import sys
+
 def get_launcher_name():
     return "nydus-launcher"
 
 def get_launcher_version():
     return "2.2.1"
+
+def get_version_arg():
+    return "--version"
+
+"""
+This function checks if the currently running program
+was given the 'show version' argument (the string
+returned by get_version_arg) in the first position.
+If so, it prints the current Nydus Launcher
+version and exits.
+This function access sys.argv, and will exit
+the entire program if the version was requested.
+If the version was not requested, it returns
+and allows the caller to proceed normally.
+"""
+def request_version():
+    if len(sys.argv[1]) > 1:
+        arg = sys.argv[1]
+        if arg == get_version_arg():
+            print(get_launcher_version())
+            exit(0)
