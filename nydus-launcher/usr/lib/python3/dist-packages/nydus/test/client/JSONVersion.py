4514227a5c9b25f6f6f08e47d45fd6b3903d7e67
Christopher Irving <the.earthlord3@gmail.com>
2025-07-29 13:39:16 +1000

    Adding rule resolve tests

---
+++ nydus-launcher/usr/lib/python3/dist-packages/nydus/test/client/JSONVersion.py
@@ -0,0 +1,53 @@
+
+from nydus.client.JSONVersion import *
+
+class TestResolveRule(unittest.TestCase):
+
+    def test_linux_yes(self):
+        rules = [
+            {
+                "action": "allow",
+                "os": {
+                    "name": "linux"
+                }
+            }
+        ]
+
+        self.assertTrue(JSONVersion.resolve_rule(rules))
+
+    def test_mac_no(self):
+        rules = [
+            {
+                "action": "allow",
+                "os": {
+                    "name": "osx"
+                }
+            }
+        ]
+        self.assertFalse(JSONVersion.resolve_rule(rules))
+
+    def test_windows_no(self):
+        rules = [
+            {
+                "action": "allow",
+                "os": {
+                    "name": "windows"
+                }
+            }
+        ]
+
+        self.assertFalse(JSONVersion.resolve_rule(rules))
+
+    def test_x86_yes(self):
+        rules = [
+            {
+                "action": "allow",
+                "os": {
+                    "arch": "x86"
+                }
+            }
+        ]
+        self.assertTrue(JSONVersion.resolve_rule(rules))
+
+if __name__ == "__main__":
+    unittest.main()
